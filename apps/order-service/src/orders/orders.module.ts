43b2322fe44ceb55874d2732139a130712671e50
Rouhan Faisal <rouhan.faisal1@gmail.com>
2026-09-26 02:47:32 +0500

    basic scaffolding for kafka order service client and consumer, docker file for kafka instance

--- apps/order-service/src/orders/orders.module.ts
+++ apps/order-service/src/orders/orders.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { OrdersController } from './orders.controller.js';
 import { OrdersService } from './orders.service.js';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_CLIENT',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'order-service',
+            brokers: ['localhost:9092']
+          },
+          consumer: {
+            groupId: 'order-service-consumer',
+          }
+        }
+      }
+    ])
+  ],
   controllers: [OrdersController],
   providers: [OrdersService]
 })
